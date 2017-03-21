8fa939c62c75517c8e76376edfd437dc29f67058
Timo Klostermann <t.klostermann@neusta.de>
2017-03-21 11:56:47 +0100

    initial hybris commit

---
+++ hybris/bin/custom/springer/springerfacades/gensrc/com/springer/hybris/facades/constants/GeneratedSpringerFacadesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 21.03.2017 08:40:15                         ---
+ * ----------------------------------------------------------------
+ */
+package com.springer.hybris.facades.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedSpringerFacadesConstants
+{
+	public static final String EXTENSIONNAME = "springerfacades";
+	
+	protected GeneratedSpringerFacadesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ hybris/bin/custom/springer/springersaporderfulfilment/gensrc/com/springer/hybris/saporderfulfillment/constants/GeneratedSpringersaporderfulfilmentConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 21.03.2017 08:40:15                         ---
+ * ----------------------------------------------------------------
+ */
+package com.springer.hybris.saporderfulfillment.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedSpringersaporderfulfilmentConstants
+{
+	public static final String EXTENSIONNAME = "springersaporderfulfilment";
+	
+	protected GeneratedSpringersaporderfulfilmentConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
