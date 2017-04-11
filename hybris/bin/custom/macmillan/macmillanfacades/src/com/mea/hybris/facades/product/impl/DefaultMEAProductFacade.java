ba9c9b3f5c8b3790b227f5b2d0368dafabe949c0
Heilig, Andreas <andreas.heilig@sybit.de>
2017-04-11 08:48:08 +0200

    removed mea product facade and populator

--- hybris/bin/custom/macmillan/macmillanfacades/src/com/mea/hybris/facades/product/impl/DefaultMEAProductFacade.java
+++ hybris/bin/custom/macmillan/macmillanfacades/src/com/mea/hybris/facades/product/impl/DefaultMEAProductFacade.java
@@ -1,70 +1,12 @@
 package com.mea.hybris.facades.product.impl;
 
-import com.mea.hybris.facades.data.MEAProductData;
 import com.mea.hybris.facades.product.MEAProductFacade;
-import com.springernature.hybris.springernaturecore.model.MEAProductModel;
-import com.springernature.hybris.springernaturecore.service.product.MEAProductService;
-import de.hybris.platform.commercefacades.product.ProductOption;
-import de.hybris.platform.converters.ConfigurablePopulator;
-import de.hybris.platform.servicelayer.dto.converter.Converter;
-import de.hybris.platform.servicelayer.exceptions.UnknownIdentifierException;
-import org.springframework.beans.factory.annotation.Required;
-
-import java.util.Collection;
 
 /**
  * Created by tklostermann on 04.04.2017.
  */
 public class DefaultMEAProductFacade implements MEAProductFacade {
-
-    private MEAProductService meaProductService;
-
-    private Converter<MEAProductModel, MEAProductData> productConverter;
-    private ConfigurablePopulator<MEAProductModel, MEAProductData, ProductOption> productConfiguredPopulator;
-
-    @Override
-    public MEAProductData getProductForCodeAndOptions(String code, Collection<ProductOption> options) throws UnknownIdentifierException, IllegalArgumentException {
-        MEAProductModel product = getMeaProductService().getProductForCode(code);
-        return getProductForOptions(product, options);
-    }
-
-    @Override
-    public MEAProductData getProductForOptions(final MEAProductModel productModel, final Collection<ProductOption> options)
-    {
-        final MEAProductData productData = getProductConverter().convert(productModel);
-
-        if (options != null)
-        {
-            getProductConfiguredPopulator().populate(productModel, productData, options);
-        }
-
-        return productData;
-    }
-
-    public MEAProductService getMeaProductService() {
-        return meaProductService;
-    }
-
-    @Required
-    public void setMeaProductService(MEAProductService meaProductService) {
-        this.meaProductService = meaProductService;
-    }
-
-    public Converter<MEAProductModel, MEAProductData> getProductConverter() {
-        return productConverter;
-    }
-
-    @Required
-    public void setProductConverter(Converter<MEAProductModel, MEAProductData> productConverter) {
-        this.productConverter = productConverter;
-    }
-
-    public ConfigurablePopulator<MEAProductModel, MEAProductData, ProductOption> getProductConfiguredPopulator() {
-        return productConfiguredPopulator;
-    }
-
-    @Required
-    public void setProductConfiguredPopulator(ConfigurablePopulator<MEAProductModel, MEAProductData, ProductOption> productConfiguredPopulator) {
-        this.productConfiguredPopulator = productConfiguredPopulator;
-    }
+    /**
+     * MEA Facade implementation here
+     */
 }
