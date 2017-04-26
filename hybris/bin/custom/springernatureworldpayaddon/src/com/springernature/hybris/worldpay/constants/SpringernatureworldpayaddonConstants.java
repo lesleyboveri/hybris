f464e484ea34795a44bc0db921013e3ca78c3836
Timo Klostermann <t.klostermann@neusta.de>
2017-04-26 15:02:09 +0200

    -Created springernatureworldpayaddon as source of configuration for worldpay. -Created springernatureassistedservicestorefront (addon) to import missing Assisted Service Restrictions used by worldpay. The original "assisstedservicestorefront" addon didnt provide these impex files in the "template" folder. -Configured localextensions.xml with new addons and addoninstall commands

---
+++ hybris/bin/custom/springernatureworldpayaddon/src/com/springernature/hybris/worldpay/constants/SpringernatureworldpayaddonConstants.java
@@ -0,0 +1,26 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2017 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.springernature.hybris.worldpay.constants;
+
+/**
+ * Global class for all Springernatureworldpayaddon constants. You can add global constants for your extension into this class.
+ */
+public final class SpringernatureworldpayaddonConstants extends GeneratedSpringernatureworldpayaddonConstants
+{
+	public static final String EXTENSIONNAME = "springernatureworldpayaddon";
+
+	private SpringernatureworldpayaddonConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
